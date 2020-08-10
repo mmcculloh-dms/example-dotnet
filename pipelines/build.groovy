638ca445f442c7f8b4c8cf7b176d34b07ca0979f
Mark McCulloh <mark.mcculloh@powerdms.com>
2020-08-10 16:25:28 -0400

    u

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -10,55 +10,45 @@ pipeline {
       }
     }
 
-    stage("Changes Detected") {
-      stages {
-        stage("Build") {
+    stage("Test") {
+      parallel {
+        stage("Static Code Analysis") {
           steps {
-            echo 'Build App'
-          }
-        }
-
-        stage("Test") {
-          parallel {
-            stage("Static Code Analysis") {
-              steps {
-                echo "Run static code analysis on DemoApp"
-                sleep 2
-              }
-            }
-
-            stage("Unit Tests") {
-              steps {
-                echo "Run unit tests on DemoApp"
-                sleep 5
-              }
-            }
-
-            stage("Integration Tests") {
-              steps {
-                echo "Run integration tests on DemoApp"
-                sleep 3
-              }
-            }
-
-            stage("E2E Test") {
-              steps {
-                echo "Run E2E tests on DemoApp"
-                CoolFunctionality()
-              }
-            }
-          }
-        }
-
-        stage("Package") {
-          when {
-            branch 'master'
-          }
-          steps {
-            echo "Package DemoApp and ship to payload bay."
+            echo "Run static code analysis on DemoApp"
             sleep 2
           }
         }
+
+        stage("Unit Tests") {
+          steps {
+            echo "Run unit tests on DemoApp"
+            sleep 5
+          }
+        }
+
+        stage("Integration Tests") {
+          steps {
+            echo "Run integration tests on DemoApp"
+            sleep 3
+          }
+        }
+
+        stage("E2E Test") {
+          steps {
+            echo "Run E2E tests on DemoApp"
+            CoolFunctionality()
+          }
+        }
+      }
+    }
+
+    stage("Package") {
+      when {
+        branch 'master'
+      }
+      steps {
+        echo "Package DemoApp and ship to payload bay."
+        sleep 2
       }
     }
 
