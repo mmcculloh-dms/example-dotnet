8e335aee63239ed1250819476b613fe57198528a
Mark McCulloh <mark.mcculloh@powerdms.com>
2020-08-14 15:49:51 -0400

    test

---
+++ pipelines/fancy.groovy
@@ -0,0 +1,62 @@
+@Library('vab') _
+
+pipeline {
+  agent any
+
+  stages {
+    stage("Build") {
+      steps {
+        echo "Build"
+      }
+    }
+
+    stage("Test") {
+      parallel {
+        stage("Static Code Analysis") {
+          steps {
+            echo "Run static code analysis on DemoApp"
+            sleep 2
+          }
+        }
+
+        stage("Unit Tests") {
+          steps {
+            echo "Run unit tests on DemoApp"
+            sleep 5
+          }
+        }
+
+        stage("Integration Tests") {
+          steps {
+            echo "Run integration tests on DemoApp"
+            sleep 3
+          }
+        }
+
+        stage("E2E Test") {
+          steps {
+            echo "Run E2E tests on DemoApp"
+            CoolFunctionality()
+          }
+        }
+      }
+    }
+
+    stage("Package") {
+      when {
+        branch 'master'
+      }
+      steps {
+        echo "Package DemoApp and ship to payload bay."
+        sleep 2
+      }
+    }
+
+    stage("Finalize") {
+      steps {
+        Finalize()
+      }
+    }
+    
+  }
+}
